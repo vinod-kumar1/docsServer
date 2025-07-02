f45b927ec931bec3eb2fadc4cf9d2aee80dc88bb
vinod-kumar1 <vinu22149@gmail.com>
2025-07-02 18:55:32 +0530

    added multi client connect support by storing docId in the client socket object

---
+++ src/server.js
@@ -0,0 +1,62 @@
+import https from "node:http";
+import readline from "node:readline";
+import { stdin as input, stdout as output } from "node:process";
+import { WebSocketServer } from "ws";
+import mysql from "mysql2";
+import { dbConnect } from "../PRIVATEKEYS.js";
+import { v4 } from "uuid";
+
+var connection = mysql.createConnection(dbConnect);
+
+connection.query("select * from Users", (err, res, fields) => {
+  console.log(res);
+});
+
+let httpServer = https.createServer((req, res) => {
+  // if (req.url == "/home") res.end("Home");
+
+  if (req.url == "/createDoc" && req.method == "POST") {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    let docId = v4();
+    connection.query(
+      `insert into googlydocs (docid , content) values ('${docId}' , '');`,
+      (err, response) => {
+        if (err) res.end(JSON.stringify(err.message));
+        else {
+          res.end(JSON.stringify(docId));
+        }
+      }
+    );
+  }
+});
+let rl = readline.createInterface({ input, output });
+let wss = new WebSocketServer({ server: httpServer });
+
+wss.on("connection", (ws) => {
+  ws.on("message", (data) => {
+    let res = JSON.parse(data);
+    console.log("res", res);
+    if (res.type == "docId") ws.docId = res.docId;
+  });
+
+  rl.on("line", (input) => {
+    wss.clients.forEach((socket) => {
+      if (socket.OPEN) {
+        socket.send(
+          JSON.stringify({ type: "message", payload: "Server> " + input })
+        );
+      }
+    });
+  });
+
+  ws.on("message", (data) => {
+    let inp = JSON.parse(data);
+    console.log(inp);
+    wss.clients.forEach((socket) => {
+      if (socket != ws && socket.docId == ws.docId)
+        socket.send(JSON.stringify(inp));
+    });
+  });
+});
+
+httpServer.listen(8000, () => console.log("Listening on 8000"));
